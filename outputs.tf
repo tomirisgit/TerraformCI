d022b800148578a7a85c74f55cb3f629d45d6ca4
tomirisgit <tom.bannister@iris.co.uk>
2025-07-11 10:41:27 +0100

    added main, output and vars

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "storange_account_id" {
+    value = azurerm_storage_account.securestorage.id
+    description = "The ID of the Azure Storage Account created for remote state storage."
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+      type = string
+      description = "The name of the resource group where the storage account will be created."
+}
+
+variable "location" {
+      type = string
+      description = "The Azure region where the storage account will be created."
+}
+
+variable "storage_account_name" {
+      type = string
+      description = "The name of the storage account to be created."
+}
+
+variable "environment" {
+      type = string
+      description = "The environment for the storage account, e.g., Production, Development."
+}
